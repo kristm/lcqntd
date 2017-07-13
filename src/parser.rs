6b52a790936841913e2733d5e7621202057a0d17
Krist Menina <krist@quipper.com>
2017-07-13 19:48:55 +0800

    complete parser test case

--- src/parser.rs
+++ src/parser.rs
@@ -4,8 +4,7 @@ use regex::Captures;
 // enum Subs {
 
 pub fn parse_line(line: &str) -> Captures {
-    //"((?:\d\d?:){2}\d{2}\.\d{2}),((?:\d\d?:){2}\d{2}\.\d{2}),Default[\d\,]+(?:\{\\?([a-z\d\\]*(?:\([0-9\,\-]*\))?)?\})?([^\{\}]*)$"
-    let re = Regex::new(r"(?P<starttime>(\d\d?:){2}\d{2}\.\d{2}),(?P<endtime>(\d\d?:){2}\d{2}\.\d{2}),Default,,[\d,]+(\{\\?([a-z\d\\]*(\([0-9,-]*\))?)?\})?(?P<dialogue>[^\{\}]*)$").unwrap();
+    let re = Regex::new(r"(?P<starttime>(?:\d\d?:){2}\d{2}\.\d{2}),(?P<endtime>(?:\d\d?:){2}\d{2}\.\d{2}),Default,,[\d,]+(?P<formatting>\{\\?(?:[a-z\d\\]*(?:\([0-9,-]*\))?)?\})?(?P<dialogue>[^\{\}]*)$").unwrap();
     let captures = re.captures(line).unwrap();
     captures
 }
